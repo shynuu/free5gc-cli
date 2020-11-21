22bb65f7c11bea0927583279226c28bdee5563f7
Youssouf Drif <drif.youssouf@protonmail.ch>
2020-11-21 21:57:18 +0100

    Clean main func

--- freecli.go
+++ freecli.go
@@ -1,45 +1,13 @@
 package main
 
 import (
-	"fmt"
 	"free5gc-cli/freecli"
-	"free5gc-cli/lib/u32"
 )
 
 // load configuration file.yaml
 // load ue file.yaml
 
 func main() {
-
-	var U32 = u32.U32{Protocols: []u32.Protocol{
-		&u32.IPV4Header{
-			Source:      "10.200.200.1",
-			Destination: "10.200.200.102",
-			Protocol:    u32.PROTO_UDP,
-			Set: &u32.IPV4Fields{
-				Source:      true,
-				Destination: true,
-				Protocol:    true,
-			},
-		},
-		&u32.UDPHeader{
-			SourcePort:      2152,
-			DestinationPort: 2152,
-			Set: &u32.UDPFields{
-				SourcePort:      true,
-				DestinationPort: true,
-			},
-		},
-		&u32.GTPv1Header{
-			TEID: 1,
-			Set:  &u32.GTPv1Fields{TEID: true},
-		},
-	},
-	}
-
-	U32.BuildPacket()
-	result := U32.BuildCommand()
-	fmt.Println("sudo iptables -t mangle -A POSTROUTING " + result + " -j DSCP --set-dscp 63")
 
 	freecli.Initialize()
 	freecli.Run()
--- lib/u32/u32.go
+++ lib/u32/u32.go
@@ -33,3 +33,33 @@ func (u32 *U32) BuildMatches() string {
 func (u32 *U32) BuildCommand() string {
 	return "-m u32 --u32 " + "\"" + u32.BuildMatches() + "\""
 }
+
+// var U32 = u32.U32{Protocols: []u32.Protocol{
+// 	&u32.IPV4Header{
+// 		Source:      "10.200.200.1",
+// 		Destination: "10.200.200.102",
+// 		Protocol:    u32.PROTO_UDP,
+// 		Set: &u32.IPV4Fields{
+// 			Source:      true,
+// 			Destination: true,
+// 			Protocol:    true,
+// 		},
+// 	},
+// 	&u32.UDPHeader{
+// 		SourcePort:      2152,
+// 		DestinationPort: 2152,
+// 		Set: &u32.UDPFields{
+// 			SourcePort:      true,
+// 			DestinationPort: true,
+// 		},
+// 	},
+// 	&u32.GTPv1Header{
+// 		TEID: 1,
+// 		Set:  &u32.GTPv1Fields{TEID: true},
+// 	},
+// },
+// }
+
+// U32.BuildPacket()
+// result := U32.BuildCommand()
+// fmt.Println("sudo iptables -t mangle -A POSTROUTING " + result + " -j DSCP --set-dscp 63")
