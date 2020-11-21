b4994ac39d265b997e5513a4878a48010007c506
Youssouf Drif <drif.youssouf@protonmail.ch>
2020-11-21 21:45:04 +0100

    Update model_sm_policy_snssai_data.go

--- lib/openapi/models/model_sm_policy_snssai_data.go
+++ lib/openapi/models/model_sm_policy_snssai_data.go
@@ -11,6 +11,6 @@ package models
 
 // Contains the SM policy data for a given subscriber and S-NSSAI.
 type SmPolicySnssaiData struct {
-	Snssai          *Snssai                    `json:"snssai" bson:"snssai"`
-	SmPolicyDnnData map[string]SmPolicyDnnData `json:"smPolicyDnnData,omitempty" bson:"smPolicyDnnData"`
+	Snssai          *Snssai                    `json:"snssai" bson:"snssai" yaml:"snssai"`
+	SmPolicyDnnData map[string]SmPolicyDnnData `json:"smPolicyDnnData,omitempty" bson:"smPolicyDnnData" yaml:"smPolicyDnnData,omitempty"`
 }
